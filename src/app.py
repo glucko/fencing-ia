828639077b3ff352bcc7522293e6eb2abc3cd52b
Benjamin Gluck <60188659+glucko@users.noreply.github.com>
2023-02-03 16:12:19 +0000

    add skeleton of file uploading

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from models import db
 from cli import create_db, print_db, add_comp, add_comp_to_tourn, add_tournament
 import os
@@ -18,9 +18,13 @@ app.cli.add_command(add_tournament)
 db.init_app(app)
 
 # create route
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        image = request.form['photo']
+        print(image)
     
 # run the app
 if __name__ == '__main__':
