c69309797d00016178f5d412c117d98b86b25e1a
Benjamin Gluck <60188659+glucko@users.noreply.github.com>
2023-03-01 16:11:18 +0000

    add fencer to tourn logic

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, Competitor, Tournament
 from cli import create_db, print_db, add_comp, add_comp_to_tourn, add_tournament
 import os
 
@@ -28,10 +28,20 @@ def hello_world():
 
 @app.route('/manual_entry', methods=['POST'])
 def manual_fencer_entry():
+    tourn_name = request.form['tournament_name']
+    tourn = Tournament.query.filter(Tournament.name == tourn_name).first()
+    if tourn is None:
+        tourn = Tournament(name=tourn_name)
+
     for key, value in range(1, len(request.form), 2):
-        print(key, value)
+        if 'name' in key:
+            fencer = Competitor.query.filter(Competitor.name == value).first()
+        else:
+            fencer = Competitor(name=value)
+    fencer.tournaments.append(tourn)
+    db.session.commit()
     
-    return 'ok'
+    return 'Successfully added fencer', 200
     
 # run the app
 if __name__ == '__main__':
