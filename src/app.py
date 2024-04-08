1256ae958a6ca54baa403a708bedebb155c278a6
Benjamin Gluck <60188659+glucko@users.noreply.github.com>
2024-04-07 22:00:31 -0400

    fix secret

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'asdfasdfwerqewgfgzvzxc'
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+os.path.join(basedir, "tmp", "data.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
